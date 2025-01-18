511d4bb0ec78f387276f343f4b35bf79f141eb99
alfa0302 <121997572+alfa0302@users.noreply.github.com>
2025-01-18 15:08:32 +0530

    Update showPageMap.js
    Added tile provider attribute

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -5,7 +5,7 @@ var map = L.map("map", {
 }).setView([latitude, longitude], 10);
 L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
   maxZoom: 19,
-  // attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([latitude, longitude]).addTo(map);
 marker.bindPopup(`<b>${campgroundObject.title}</b>`).openPopup();
